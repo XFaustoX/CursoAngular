2a25d1764904698d0d2c987865ff10a704d8e8c5
XFaustoX <pedro.borroni.m@gmail.com>
2024-10-18 14:56:08 -0300

    18-10 14:50

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'Fausto DEV';
-  public counter: number = 10;
-
-  increaseBy(value :number):void{
-    this.counter += value; //metodo para aumentar y reducir el contador
-  }
-
-  resetCounter(value: number):void{
-    this.counter = value;
-  }
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 // para crear mas rapido usar a- y elegir
+//para hacer back sticks ` altGr + }
 @Component({
   selector: 'app-counter',
-  template: '<h1>Hola Counter</h1>'
+  template: `
+    <h3>{{counter}}</h3>
+
+    <button (click)="increaseBy(+1)">+1</button>
+    <button (click)="resetCounter(10)">Reset Counter</button>
+    <button (click)="increaseBy(-1)">-1</button>
+  `
 })
 
 export class CounterComponent {
-  constructor() { }
+  public counter: number = 10;
+
+  increaseBy(value :number):void{
+    this.counter += value; //metodo para aumentar y reducir el contador
+  }
+
+  resetCounter(value: number):void{
+    this.counter = value; //metodo para resetear a 10 el contador
+  }
 }
